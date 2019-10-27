8a1fb61a8e76932d559faa04be84fde916788cc2
Orlogskapten <wenceslas.sanchez@gmail.com>
2019-10-27 19:00:23 +0100

    Amélioration fonctionnalité de recherche

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -15,7 +15,7 @@ echo "Voici le répertoire $rep: "
 echo "$localisation"
 echo ""
 #recherche du pattern dans $rep
-resultat_pattern=$(find $localisation -name $1 -exec grep -iLER $2 {} \;)
+resultat_pattern=$(find $localisation -name "$1" | xargs grep -iERl $1)
 
 echo "Voici les résultats correspondant au pattern renseigné: "
 echo "$resultat_pattern"
